95798d3b3126f25057873e10b0e3f3da5d444ca2
kianamcdaniel <40674230+kianamcdaniel@users.noreply.github.com>
2018-11-05 18:05:39 -0600

    Update prog-3b.c

--- prog-3b.c
+++ prog-3b.c
@@ -5,15 +5,14 @@
 //  Created by Kiana McDaniel on 11/1/18.
 //
 
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <pthread.h>
 #include <math.h>
 #include <unistd.h>
 
 int fund = 4000;
-int award = 0;
 int total_award = 0;
+char stu = 'A';
 
 pthread_mutex_t mutex;
 
@@ -24,12 +23,12 @@ int main(){
     pthread_t tid[3];
     pthread_setconcurrency(3);
     
-    for(int i = 0; i < 3; i++){
+    int i;
+    for(i = 0; i < 3; i++){
         pthread_create(&tid[i], NULL, (void *(*)(void *))calc_fund, NULL);
-        sleep(1);
     }
     
-    for(int i = 0; i < 3; i++)
+    for(i = 0; i < 3; i++)
         pthread_join(tid[i], NULL);
     
     printf("TOTAL: $%d\n", total_award);
@@ -37,16 +36,25 @@ int main(){
 
 void *calc_fund(){
     
+    char stu_id;
     pthread_mutex_lock(&mutex);
-    
-    while(fund != 0){
-        award = ceil(fund * 0.25);
-        fund = fund - award;
-        printf("%d\n", award);
-        total_award += award;
-    }
-    
+    stu_id = stu;
+    stu++;
     pthread_mutex_unlock(&mutex);
+
+    while(fund > 0){
+        
+        pthread_mutex_lock(&mutex);
+        
+        int award = ceil(fund * 0.25);
+        fund -= award;
+        printf("%c = %d\n", stu_id, award);
+        total_award += award;
+        
+        pthread_mutex_unlock(&mutex);
+        
+        sleep(1);
+    }
     
     return NULL;
 }
